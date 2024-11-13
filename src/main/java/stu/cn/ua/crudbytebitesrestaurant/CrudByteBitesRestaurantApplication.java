aada18c8e86a3891892dfe716f3434a91b1e8ce1
Denys Lysenok <cerobreath@gmail.com>
2024-11-13 12:44:11 +0200

    Creating the application structure

---
+++ src/main/java/stu/cn/ua/crudbytebitesrestaurant/CrudByteBitesRestaurantApplication.java
@@ -0,0 +1,13 @@
+package stu.cn.ua.crudbytebitesrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudByteBitesRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudByteBitesRestaurantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stu/cn/ua/crudbytebitesrestaurant/CrudByteBitesRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package stu.cn.ua.crudbytebitesrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudByteBitesRestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
