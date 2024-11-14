90e27d2a05a57ab48910bf08904c72a5d89c546c
Dylan <cerobreath@gmail.com>
2024-11-14 13:48:55 +0200

    Complete rebuild of the project for easy execution of the 3rd laboratory work

---
+++ src/main/java/stu/cn/ua/crudbytebitesrestaurant/Application.java
@@ -0,0 +1,21 @@
+package stu.cn.ua.crudbytebitesrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/test/java/stu/cn/ua/crudbytebitesrestaurant/ApplicationTests.java
+++ src/test/java/stu/cn/ua/crudbytebitesrestaurant/ApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CrudByteBitesRestaurantApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
